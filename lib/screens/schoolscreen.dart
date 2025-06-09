ff9293cf7f80d56be7a785c1d1e51269d30ab1e8
Chilaka Bhargavi <bhargavichilakaaa@gmail.com>
2025-06-09 09:55:13 +0530

    Update schoolscreen.dart

--- lib/screens/schoolscreen.dart
+++ lib/screens/schoolscreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Schoolscreen extends StatefulWidget {
-  const Schoolscreen ({super.key});
+  const Schoolscreen({super.key});
 
   @override
   _SchoolscreenPageState createState() => _SchoolscreenPageState();
@@ -9,28 +9,29 @@ class Schoolscreen extends StatefulWidget {
 
 class _SchoolscreenPageState extends State<Schoolscreen> {
   String? _selectedOption = 'Default';
+  String? _selectedDistrict = 'Default';
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  final List<String> _dropdownOptions = ['Default', 'School A', 'School B','School C'];
+  final List<String> _dropdownOptions = ['Default', 'School A', 'School B', 'School C'];
+  final List<String> _districtOptions = ['Default', 'Dist X', 'Dist Y', 'Dist Z'];
+
   void _submitForm() {
-    if ( _formKey.currentState!.validate() && _selectedOption != 'Default') {
-      // All validations passed
+    if (_formKey.currentState!.validate() &&
+        _selectedOption != 'Default' &&
+        _selectedDistrict != 'Default') {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Joined successfully!')),
       );
     }
-
-
-    // Submit logic here
   }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text('School Mapping',style: TextStyle(color: Colors.white,fontSize: 24),),
-
+        title: Text('School Mapping', style: TextStyle(color: Colors.white, fontSize: 24)),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back,color: Colors.white,),
+          icon: Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () => Navigator.pop(context),
         ),
         backgroundColor: Colors.blue,
@@ -40,46 +41,82 @@ class _SchoolscreenPageState extends State<Schoolscreen> {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              Image.asset('assets/images/school.png',height: 300,width: 300,),
-              Text('Choose one of the ATL schools:',style: TextStyle(fontSize: 19),),
-              SizedBox(height: 16,),
-              SizedBox(
-                width: 300,
-                child: Form(
-                  key: _formKey,
-                  child: DropdownButtonFormField<String>(
-                    value: _selectedOption,
-                    items: _dropdownOptions.map((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    decoration: InputDecoration(border: OutlineInputBorder(),),
-                    onChanged: (newValue) {
-                      setState(() {
-                        _selectedOption = newValue!;
-                      });
-                    },
-                    validator: (value) {
-                      if (value == null || value == 'Default') {
-                        return 'Please select a valid school';
-                      }
-                      return null;
-                    },
-                  ),
+              Image.asset('assets/images/school.png', height: 300, width: 300),
+              Text('Choose your District and ATL School:', style: TextStyle(fontSize: 19)),
+              SizedBox(height: 16),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    // District Dropdown
+                    SizedBox(
+                      width: 300,
+                      child: DropdownButtonFormField<String>(
+                        value: _selectedDistrict,
+                        items: _districtOptions.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        decoration: InputDecoration(
+                          labelText: 'Choose District',
+                          border: OutlineInputBorder(),
+                        ),
+                        onChanged: (newValue) {
+                          setState(() {
+                            _selectedDistrict = newValue!;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null || value == 'Default') {
+                            return 'Please select a valid District';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+
+                    SizedBox(height: 20),
+
+                    // School Dropdown
+                    SizedBox(
+                      width: 300,
+                      child: DropdownButtonFormField<String>(
+                        value: _selectedOption,
+                        items: _dropdownOptions.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        decoration: InputDecoration(
+                          labelText: 'ATL School Mapping',
+                          border: OutlineInputBorder(),
+                        ),
+                        onChanged: (newValue) {
+                          setState(() {
+                            _selectedOption = newValue!;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null || value == 'Default') {
+                            return 'Please select a valid school';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
-
               SizedBox(height: 40),
               Center(
                 child: ElevatedButton(
                   onPressed: _submitForm,
-
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.blue,
-                    padding:
-                    EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(8),
                     ),
@@ -91,8 +128,6 @@ class _SchoolscreenPageState extends State<Schoolscreen> {
                 ),
               ),
               SizedBox(height: 30),
-
-
             ],
           ),
         ),
